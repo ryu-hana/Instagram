065f090929b723d523dadc4fd9ca4f7e9db65e10
HONDA <honda0211.no9@gmail.com>
2021-06-06 04:08:57 +0900

    いいねボタンの実装まで完了

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by SugiuraArisa on 2021/06/06.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
